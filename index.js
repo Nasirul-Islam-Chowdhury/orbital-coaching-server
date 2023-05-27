0aadb57a7ba6f103abc9eeac7f63ac4a06794878
nicchy123 <nasirchy252@gmail.com>
2023-05-27 22:37:08 +0600

    review system created

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 app.use(cors())
 require('dotenv').config();
-
+app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.tutor_db_user_name}:${process.env.tutor_db_user_pass}@cluster0.bbqqyyb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -31,7 +31,22 @@ async function run() {
         const services = await cursor.toArray();
         res.send(services)
     })
-    
+
+    app.post('/reviews', async(req, res)=>{
+      const query = req.body;
+      const review = await reviewsCollection.insertOne(query)
+      res.send(review)
+    })
+
+    app.get('/reviews/:name', async(req,res)=>{
+      const teacherName = req.params.name;
+      let query = {teacherName: teacherName};
+      const cursor = await reviewsCollection.find(query);
+      const reviews = await cursor.toArray();
+      console.log(reviews)
+      res.send(reviews)
+    })
+
     app.get("/serviceDetails/:id", async (req, res) => {
       const id = req.params.id;
         const query = {_id: new ObjectId(id)};
